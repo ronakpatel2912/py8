620e2bd44e55951fee7b4280a599a027319c22f3
The EasyLearn Academy <theeasylearn@users.noreply.github.com>
2021-12-16 20:36:22 +0530

    comparison

---
+++ comparison_operator.py
@@ -0,0 +1,9 @@
+#example of relational operators
+a = int(input("Enter first value"))
+b = int(input("Enter second value"))
+print(a==b) #true
+print(a!=b)
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
